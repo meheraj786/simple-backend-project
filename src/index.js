644066a8507218835e029f57442cb7a172e4b5d4
meheraj786 <meherajhosen786@gmail.com>
2025-08-31 11:13:51 +0600

    bd connected

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv'
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import express from "express";
+
+dotenv.config({ path: './' })
+
+const app = express(); 
+
+;(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log('Connected to MongoDB successfully');
+    
+    const server = app.listen(process.env.PORT || 3000, () => {
+        console.log(`App is running on port ${process.env.PORT || 3000}`);
+    });
+    
+    server.on('error', (err) => {
+        console.log(`Server error: ${err}`);
+    });
+    
+  } catch (error) {
+    console.error("Error DB: ", error);
+    process.exit(1);
+  }
+})();
