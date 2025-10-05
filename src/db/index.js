223803d483673ac7d45be5d90024bd60de0774b9
meheraj786 <meherajhosen786@gmail.com>
2025-10-05 21:29:43 +0600

    multer add

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,6 @@ const connectDB = async () => {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
         
-        // Database connection events
         mongoose.connection.on('error', (err) => {
             console.error('Database connection error:', err);
         });
